287e0e013792ee713195e964e38d441ed62570ea
quanxuantruong <quanxuantruong2004@gmail.com>
2024-11-03 09:43:45 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+from function import *
+
+cap = cv2.VideoCapture(1400)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
