043cfd4a80546768071a44be259c18ebceb84325
quanxuantruong <quanxuantruong2004@gmail.com>
2024-11-08 20:57:57 +0700

    update

--- main.py
+++ main.py
@@ -70,22 +70,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence
         if keyboard.is_pressed(' '):
             sentence, sequence , grammar_result = [], [], []
             
-        # # Check if the list is not empty
-        # if sentence:
-        #     # Capitalize the first word of the sentence
-        #     sentence[0] = sentence[0].capitalize()
-
-        #  # Check if the sentence has at least two elements
-        # if len(sentence) >= 2:
-        #     # Check if the last element of the sentence belongs to the alphabet (lower or upper cases)
-        #     if sentence[-1] in string.ascii_lowercase or sentence[-1] in string.ascii_uppercase:
-        #         # Check if the second last element of sentence belongs to the alphabet or is a new word
-        #         if sentence[-2] in string.ascii_lowercase or sentence[-2] in string.ascii_uppercase or (sentence[-2] not in actions and sentence[-2] not in list(x.capitalize() for x in actions)):
-        #             # Combine last two elements
-        #             sentence[-1] = sentence[-2] + sentence[-1]
-        #             sentence.pop(len(sentence) - 2)
-        #             sentence[-1] = sentence[-1].capitalize()
-
         # Perform grammar check if "Enter" is pressed
         if keyboard.is_pressed('enter'):
             # Record the words in the sentence list into a single string
@@ -102,12 +86,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence
             cv2.putText(image, grammar_result, (150, 470),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (144, 238, 144), 2, cv2.LINE_AA)
         else:
-            # Calculate the size of the text to be displayed and the X coordinate for centering the text on the image
-            # textsize = cv2.getTextSize(' '.join(sentence), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
-            # text_X_coord = (image.shape[1] - textsize[0]) // 2
-
-            # print(text_X_coord)
-
             # Draw the sentence on the image
             cv2.putText(image, ' '.join(sentence), (150, 470),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
